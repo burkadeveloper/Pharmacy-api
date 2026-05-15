6bd82f973a601e75f16bd1a60ebffaf4c2b0b008
burkadeveloper <burkaa16@gmail.com>
2026-05-15 20:54:43 +0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,7 @@ import {
   register,
   login,
   updateAdminProfile,
+  setupFirstAdmin
 } from "../controllers/authController.js";
 import { protect } from "../middleware/auth.js";
 const router = express.Router();
@@ -10,4 +11,5 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.put("/profile", protect, updateAdminProfile);
+router.post('/setup-first-admin', setupFirstAdmin);
 export default router;
